373bdf88abb14c45c5248dfffb6f6a2f622ca2a4
Attila Gulyas <toraritte@gmail.com>
2014-03-04 08:52:00 -1000

    Initial commit

---
+++ simple.rb
@@ -0,0 +1,17 @@
+Camping.goes :Simple
+
+module Simple::Controllers
+  class Index < R '/'
+    def get
+      'hejho'
+    end
+  end
+end
+
+module Simple::Views
+end
+
+module Simple::Models
+end
+
+
