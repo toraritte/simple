040b20c3cf241858537611e8e43896025048f990
Attila Gulyas <toraritte@gmail.com>
2014-03-21 10:17:04 -1000

    slim works although only in the same file yet
    help from Magnus Holm:
    http://www.mail-archive.com/camping-list@rubyforge.org/msg02058.html

--- simple.rb
+++ simple.rb
@@ -14,23 +14,36 @@ module Simple::Controllers
   end
 end
 
-module Simple::Views
-  def layout
-    html do
-      head do
-        title { "minek mentek az usaba?"}
-      end
-      body { self << yield }
-    end
-  end
+# module Simple::Views
+#   def layout
+#     html do
+#       head do
+#         title { "minek mentek az usaba?"}
+#       end
+#       body { self << yield }
+#     end
+#   end
 
-  def index
-    p { "Na most ennyi" }
-    img src: "https://googledrive.com/host/0B4yCEq1xUxRWYVJuQmlmWlpMX1E/IMG_2785.JPG", width: "270", height: "270"
-  end
-end
+#   def index
+#     p { "Na most ennyi" }
+#     img src: "https://googledrive.com/host/0B4yCEq1xUxRWYVJuQmlmWlpMX1E/IMG_2785.JPG", width: "270", height: "270"
+#   end
+# end
 
 module Simple::Models
 end
 
+__END__
 
+@@ layout.slim
+doctype html
+html
+  head
+    title 'minek mentek az usaba?'
+
+  body
+    ==yield
+
+@@ index.slim
+p 'Na most ennyi'
+img [src="https://googledrive.com/host/0B4yCEq1xUxRWYVJuQmlmWlpMX1E/IMG_2785.JPG" width="270" height="270"]
