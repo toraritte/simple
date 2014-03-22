53bea8a90d22aea92f4571f225a6d0bb675d3b9b
Attila Gulyas <toraritte@gmail.com>
2014-03-21 20:40:26 -1000

    Views placed in separate files
    https://github.com/judofyr/timeless used as template.
    
    Find out:
    1. __END__
       @@ bla.slim
    
       Why was it working?
    
    2. module Simple
         set :views, File.dirname(__FILE__) + '/views'
       end
    
      See question at 1. Check out camping-unabridged.rb

--- simple.rb
+++ simple.rb
@@ -4,6 +4,7 @@ require 'slim'
 Camping.goes :Simple
 
 module Simple
+  set :views, File.dirname(__FILE__) + '/views'
 end
 
 module Simple::Controllers
@@ -13,37 +14,20 @@ module Simple::Controllers
     end
   end
 end
-
-# module Simple::Views
-#   def layout
-#     html do
-#       head do
-#         title { "minek mentek az usaba?"}
-#       end
-#       body { self << yield }
-#     end
-#   end
-
-#   def index
-#     p { "Na most ennyi" }
-#     img src: "https://googledrive.com/host/0B4yCEq1xUxRWYVJuQmlmWlpMX1E/IMG_2785.JPG", width: "270", height: "270"
-#   end
-# end
-
 module Simple::Models
 end
 
-__END__
+# __END__
 
-@@ layout.slim
-doctype html
-html
-  head
-    title 'minek mentek az usaba?'
+# @@ layout.slim
+# doctype html
+# html
+#   head
+#     title 'minek mentek az usaba?'
 
-  body
-    ==yield
+#   body
+#     ==yield
 
-@@ index.slim
-p 'Na most ennyi'
-img [src="https://googledrive.com/host/0B4yCEq1xUxRWYVJuQmlmWlpMX1E/IMG_2785.JPG" width="270" height="270"]
+# @@ index.slim
+# p 'Na most ennyi'
+# img [src="https://googledrive.com/host/0B4yCEq1xUxRWYVJuQmlmWlpMX1E/IMG_2785.JPG" width="270" height="270"]
