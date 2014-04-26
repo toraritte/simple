0783408f9d5dea8388ad83f1d074154fc3005514
Attila Gulyas <toraritte@gmail.com>
2014-04-25 17:53:57 -1000

    Add config.ru
    First commit with fugitive.

--- simple.rb
+++ simple.rb
@@ -1,3 +1,6 @@
+require 'bundler'
+Bundler.setup
+
 require 'camping'
 require 'slim'
 
@@ -14,20 +17,6 @@ module Simple::Controllers
     end
   end
 end
+
 module Simple::Models
 end
-
-# __END__
-
-# @@ layout.slim
-# doctype html
-# html
-#   head
-#     title 'minek mentek az usaba?'
-
-#   body
-#     ==yield
-
-# @@ index.slim
-# p 'Na most ennyi'
-# img [src="https://googledrive.com/host/0B4yCEq1xUxRWYVJuQmlmWlpMX1E/IMG_2785.JPG" width="270" height="270"]
